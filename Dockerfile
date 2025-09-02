0b8d07d9883e5d1708737ddbe7921903689abc8e
godowski <godowski@protonmal.com>
2025-09-02 06:19:16 -0700

    Add Dockerfile and optimize GO build script

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Dockerfile.alpine
+FROM golang:1.25-alpine AS builder
+
+RUN apk update
+RUN apk add git
+
+WORKDIR /app
+COPY  build.sh go.mod go.sum main.go vanity_server.go vanity_template.go functions.go conf.go ./
+COPY .git/ ./.git
+
+RUN CGO_ENABLED=0 GOOS=linux ./build.sh
+
+FROM alpine:3.22
+
+WORKDIR /app
+COPY --from=builder /app/goovus  .
+
+CMD ["./goovus"]
--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 DATE=$(date)
 GOVERSION=$(go version)
 VERSION=$(git describe --tags --abbrev=8 --dirty --always --long)
 
-LDFLAGS=
+LDFLAGS=""
 LDFLAGS="$LDFLAGS -X 'main.Version=$VERSION'"
 LDFLAGS="$LDFLAGS -X 'main.Date=$DATE'"
 LDFLAGS="$LDFLAGS -X 'main.GoVersion=$GOVERSION'"
-go build -ldflags "$LDFLAGS"
+
+go build  -ldflags "$LDFLAGS" -o goovus .
